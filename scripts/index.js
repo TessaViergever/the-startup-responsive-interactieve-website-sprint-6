707ce7320b65a384b613a4dd92fc2169d528984f
TessaViergever <tessa.viergever@hva.nl>
2023-01-25 15:04:25 +0100

    final tutsjusss (geen idee meer wat allemaal)

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,40 @@
 
+const menuButton = document.querySelector('#button-hamburger-menu')
+const nav = document.querySelector('.main-navigation')
+
+menuButton.addEventListener('click', function() {
+    nav.classList.toggle('open')
+    menuButton.classList.toggle('open')
+})
 
 
-//Stap 1: JS koppelen aan CSS/HTML //
-let menuButton = document.querySelector ('#button-hamburger-menu') //(SELECT)//
-let menuOpensBelow = document.querySelector ('nav.open')
 
 
-//Stap 2: event selecteren (IF this...) //
-menuButton.addEventListener ('click', openMenu) //(EVENT)//
 
 
-//Stap 3: functie toewijzen (...THEN that) //
-function openMenu() {
-    menuOpensBelow.classList.toggle('nav.open') //(FUNCTION)//
-}
+
+
+
+
+// console.log(nav)
+
+
+
+
+
+
+
+// //Stap 1: JS koppelen aan CSS/HTML //
+// let menuButton = document.querySelector ('#button-hamburger-menu') //(SELECT)//
+// let menuOpensBelow = document.querySelector ('nav.open')
+
+
+// //Stap 2: event selecteren (IF this...) //
+// menuButton.addEventListener ('click', openMenu) //(EVENT)//
+
+
+// //Stap 3: functie toewijzen (...THEN that) //
+// function openMenu() {
+//     menuOpensBelow.classList.toggle('nav.open') //(FUNCTION)//
+// }
 
