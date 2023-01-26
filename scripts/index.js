ae7b7d581aaf3183a0a9ffc307891f8c1607becc
TessaViergever <tessa.viergever@hva.nl>
2023-01-26 12:56:36 +0100

    cleaning up: HTML, CSS, JS

--- scripts/index.js
+++ scripts/index.js
@@ -8,33 +8,3 @@ menuButton.addEventListener('click', function() {
 })
 
 
-
-
-
-
-
-
-
-
-// console.log(nav)
-
-
-
-
-
-
-
-// //Stap 1: JS koppelen aan CSS/HTML //
-// let menuButton = document.querySelector ('#button-hamburger-menu') //(SELECT)//
-// let menuOpensBelow = document.querySelector ('nav.open')
-
-
-// //Stap 2: event selecteren (IF this...) //
-// menuButton.addEventListener ('click', openMenu) //(EVENT)//
-
-
-// //Stap 3: functie toewijzen (...THEN that) //
-// function openMenu() {
-//     menuOpensBelow.classList.toggle('nav.open') //(FUNCTION)//
-// }
-
